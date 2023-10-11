25306744a3813de0de921b1d037617ebf53e6c7f
KrzychuK121 <KrzychuK1211@gmail.com>
2023-10-11 11:09:22 +0200

    Poprawienie i dodanie opisów metod.

--- src/gra/GameFrame.java
+++ src/gra/GameFrame.java
@@ -15,16 +15,35 @@ public interface GameFrame {
     public void clearBoard();
     /**
      * Funkcja odpowiedzialna za ustawienie wszystkich zmiennych
-     * potrzebnych do wyświetlenia g ry
+     * potrzebnych do wyświetlenia gry
      */
     public void viewGame(Board board, Player[] players);
     public void viewBoard(Board board, Player player);
     public void viewWinner(Player player);
-    // Wyświetla użytkownikowi listę pionków, którymi może wykonać ruch
+    /**
+     * Wyświetla użytkownikowi listę pionków, którymi może wykonać ruch.
+     * 
+     * @param player obiekt reprezentujący gracza wykonującego ruch.
+     * @param whichPlayer określa którym w kolejności jest nasz gracz.
+     * @param howManyEye ilość wyrzuconych oczek przez gracza podczas rzutu kostką.
+     * @param board obiekt planszy zawierający wszystkie pionki z ich położeniami.
+     *
+     * @return tablicę pionków, którymi można wykonać ruch. 
+     *	       Jeśli jest pusta oznacza, że nie ma pionków, którymi można wykonać ruch.
+     */
     public ArrayList<Counter> viewAvalibleCounters(Player player, int whichPlayer, int howManyEye, Board board);
+    /**
+     * <pre>
+     * Czyści planszę wyświetloną na ekranie a następnie wyświetla wszystkie
+     * pionki znajdujące się na planszy.
+     * </pre>
+     * 
+     * @param board plansza zawierająca wszystkie pionki do wyświetlenia wraz z ich pozycjami.
+     */
     public void viewCounters(Board board);
     /**
      * Animacja rzucania kostką (wyświetlanie po kolei ilości oczek)
+     * 
      * @return liczbę wylosowanych przez gracza oczek.
      */
     public int viewDieAnimation();
